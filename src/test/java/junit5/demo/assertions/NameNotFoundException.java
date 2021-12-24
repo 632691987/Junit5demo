80807fda1f6c1f4156bfc037a64b51801cddb5b3
Vincent <vincentzhang@outlook.es>
2021-12-24 02:16:37 +0100

    Project init

---
+++ src/test/java/junit5/demo/assertions/NameNotFoundException.java
@@ -0,0 +1,7 @@
+package junit5.demo.assertions;
+
+public class NameNotFoundException extends Exception {
+    public NameNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/junit5/demo/display/DisplayNameTest.java
@@ -0,0 +1,15 @@
+package junit5.demo.display;
+
+import org.junit.jupiter.api.Test;
+
+public class DisplayNameTest {
+
+    @Test
+    void test_spaces_ok() {
+    }
+
+    @Test
+    void test_spaces_fail() {
+    }
+
+}
